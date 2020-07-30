56cf6a864b384f5fcf4ab6816fff2321dc48c1f4
Denis Maximenko <manlok71@gmail.com>
2020-07-30 11:19:04 +0300

    Add simple movement script for pads
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Assets/Pads/VerticalMovement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalMovement : MonoBehaviour
+{
+    #region Movement
+    [Header("Movement")]
+    [SerializeField]
+    private float speed = 1f;
+    #endregion
+
+    [SerializeField]
+    private KeyCode upKey;
+    [SerializeField]
+    private KeyCode downKey;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(upKey))
+        {
+            transform.Translate(Vector3.up * speed * Time.fixedDeltaTime);
+        }
+        else if (Input.GetKey(downKey))
+        {
+            transform.Translate(-Vector3.up * speed * Time.fixedDeltaTime);
+        }
+    }
+}
