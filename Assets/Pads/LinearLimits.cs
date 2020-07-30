938c8d309416001be3f1f9cc0fe1c6607ee9c629
Denis Maximenko <manlok71@gmail.com>
2020-07-30 09:32:13 +0300

    Add class that limits pad movement on a given axis
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Assets/Pads/LinearLimits.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearLimits : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 axis = Vector3.up;
+    [SerializeField]
+    private Vector3 referencePoint = Vector3.zero;
+    [SerializeField]
+    private float maxDistance = 1;
+    [SerializeField]
+    private float minDistance = -1;
+
+    void Update()
+    {
+        CheckPosition();
+    }
+
+    void CheckPosition()
+    {
+        float distance = Vector3.Dot(axis, transform.position - referencePoint);
+        if (distance >= minDistance && distance <= maxDistance)
+        {
+            return;
+        }
+        float overstep = distance - Mathf.Clamp(distance, minDistance, maxDistance);
+        transform.Translate(-overstep * axis, Space.World);
+    }
+}
