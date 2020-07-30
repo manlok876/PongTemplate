e01342024ffa07fb1d146e73b548b77664b0f0ec
Denis Maximenko <manlok71@gmail.com>
2020-07-30 09:14:46 +0300

    Add script that limits pad movement on Y axis
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Assets/Pads/VerticalLimits.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalLimits : MonoBehaviour
+{
+    [SerializeField]
+    private float maxY = 1;
+    [SerializeField]
+    private float minY = -1;
+
+    void Update()
+    {
+        CheckPosition();
+    }
+
+    void CheckPosition()
+    {
+        float height = transform.position.y;
+        if (height >= minY && height <= maxY)
+        {
+            return;
+        }
+        float overstep = height - Mathf.Clamp(height, minY, maxY);
+        transform.Translate(-overstep * Vector3.up, Space.World);
+    }
+}
