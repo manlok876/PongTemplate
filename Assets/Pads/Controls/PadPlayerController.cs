f957fdf542810b8e353779d8b7ab9dc433667f6a
Denis Maximenko <manlok71@gmail.com>
2020-08-18 21:17:43 +0300

    Add PadPlayerController
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Assets/Pads/Controls/PadPlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadPlayerController : MonoBehaviour
+{
+    private VerticalMovement moveComponent = null;
+
+    void Awake()
+    {
+        moveComponent = GetComponent<VerticalMovement>();
+    }
+
+    void Update()
+    {
+        if (!moveComponent)
+        {
+            return;
+        }
+
+        float inputValue = Input.GetAxis("Vertical");
+
+        if (inputValue > 0)
+        {
+            moveComponent.direction = VerticalMovement.Direction.Up;
+        }
+        else if (inputValue < 0)
+        {
+            moveComponent.direction = VerticalMovement.Direction.Down;
+        }
+        else
+        {
+            moveComponent.direction = VerticalMovement.Direction.Stop;
+        }
+    }
+}
