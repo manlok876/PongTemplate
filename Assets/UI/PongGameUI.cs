3062b5c8a6bbc41c55fdf109cbb23d402fd53a0e
Denis Maximenko <manlok71@gmail.com>
2020-09-13 14:45:47 +0300

    Create a script for in-game UI
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Assets/UI/PongGameUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// In-game UI, with scores, time, etc.
+public class PongGameUI : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
