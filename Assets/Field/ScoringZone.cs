8ad3e54b40c2f6278bb3433c4ae8aedc12747b2f
Denis Maximenko <manlok71@gmail.com>
2020-09-15 22:42:19 +0300

    Create base class for scoring zones
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Assets/Field/ScoringZone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoringZone : MonoBehaviour
+{
+    public event Action OnScored;
+
+    protected void Score()
+    {
+        OnScored.Invoke();
+    }
+}
