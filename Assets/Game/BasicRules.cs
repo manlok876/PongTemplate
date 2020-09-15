ced9d15f7e314b71d63630a231162d072e21e879
Denis Maximenko <manlok71@gmail.com>
2020-09-15 22:44:03 +0300

    Add rules for basic gameplay version
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Assets/Game/BasicRules.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicRules : ScoringRules
+{
+    [SerializeField]
+    private ScoringZone player1ScoringZone = null;
+    [SerializeField]
+    private ScoringZone player2ScoringZone = null;
+
+    void Awake()
+    {
+        player1ScoringZone.OnScored += () => { PlayerScored(0); };
+        player2ScoringZone.OnScored += () => { PlayerScored(1); };
+    }
+}
