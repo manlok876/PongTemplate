a2133b48f2618cdb68c71a297d5d9680eb5c2efe
Denis Maximenko <manlok71@gmail.com>
2020-09-15 22:42:43 +0300

    Use method to invoke event in ScoringRules
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

--- Assets/Game/ScoringRules.cs
+++ Assets/Game/ScoringRules.cs
@@ -4,4 +4,9 @@ using UnityEngine;
 public class ScoringRules : MonoBehaviour
 {
     public event Action<int> OnPlayerScored;
+
+    protected void PlayerScored(int playerIndex)
+    {
+        OnPlayerScored.Invoke(playerIndex);
+    }
 }
