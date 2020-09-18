ac8f88436f61d42f0600eceefbd04cb0cbcdd106
Denis Maximenko <manlok71@gmail.com>
2020-09-18 21:39:13 +0300

    Add PauseButton script
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Assets/UI/PauseButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ingameMenu = null;
+    
+    public void OnClick()
+    {
+        PongGame game = PongGame.instance;
+
+        if (game.isPaused)
+        {
+            game.Unpause();
+            ingameMenu.SetActive(false);
+        }
+        else
+        {
+            game.Pause();
+            ingameMenu.SetActive(true);
+        }
+    }
+}
