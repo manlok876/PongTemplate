958122f853c726f7c399446c88c2ede84e90c36b
Denis Maximenko <manlok71@gmail.com>
2020-09-21 22:11:41 +0300

    Add script for ingame menu
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Assets/UI/IngameMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameMenu : MonoBehaviour
+{
+    private PongGame game => PongGame.instance;
+
+    public void PauseGame()
+    {
+        if (game.isPaused)
+        {
+            return;
+        }
+
+        gameObject.SetActive(true);
+        game.Pause();
+    }
+
+    public void ResumeGame()
+    {
+        if (!game.isPaused)
+        {
+            return;
+        }
+
+        gameObject.SetActive(false);
+        game.Unpause();
+    }
+
+    public void ReturnToMenu()
+    {
+        game.ReturnToMainMenu();
+    }
+}
