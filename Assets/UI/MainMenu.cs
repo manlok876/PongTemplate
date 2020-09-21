81ad1d71d2227bae6da4e3cbe19c06735a5c0e2d
Denis Maximenko <manlok71@gmail.com>
2020-09-21 22:05:13 +0300

    Add script for main menu functionality
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Assets/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private PongGame game => PongGame.instance;
+
+    public void StartGame()
+    {
+        game.StartGame();
+    }
+
+    public void ExitGame()
+    {
+        game.ExitGame();
+    }
+}
