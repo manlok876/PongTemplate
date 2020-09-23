ec469fbfa1f3d503c53a02f13cccefa9654ce962
Denis Maximenko <manlok71@gmail.com>
2020-09-24 00:23:57 +0300

    Add script for pong balls (initial version)
    Signed-off-by: Denis Maximenko <manlok71@gmail.com>

---
+++ Assets/Objects/Ball/PongBall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PongBall : MonoBehaviour
+{
+    [SerializeField]
+    private float startingSpeed = 1;
+
+    void Start()
+    {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = Vector2.right * startingSpeed;
+    }
+}
